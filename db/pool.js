adb5b8563f4670098a846c93b40a578e48df12f2
dark-wizz <mailwithakram@gmail.com>
2024-12-04 01:21:44 +0530

    Remove db conf

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,3 @@
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: "localhost",
-  user: "akram",
-  database: "top_users",
-  password: "sql123",
-  port: 5432,
-});
+module.exports = new Pool();
